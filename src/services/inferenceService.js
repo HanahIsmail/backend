8b0299e6d005bc0e83ba80d9a92ad12816a560ba
Siti Mumtahanah <123310988+HanahIsmail@users.noreply.github.com>
2024-12-09 11:25:25 +0800

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -6,7 +6,7 @@ let model;
 const loadModel = async () => {
     if (!model) {
         const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME;
-        const modelPath = `gs://${bucketName}/model/model.json`;
+        const modelPath = `gs://${bucketName}/model.json`;
         model = await tf.loadGraphModel(modelPath);
     }
     return model;
